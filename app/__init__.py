0690f03d3f337aebbff35a80183696e00bd94a4f
davidaryasetia <david.aryasetia@gmail.com>
2025-04-10 10:48:17 +0700

    fix the route:

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 # Initializing Application with init.py 
 from flask import Flask 
-from flask_restx import Api 
+from flask_restx import Api, Namespace, Resource
+
+from .controllers.hello_controller import hello_controller
 
 def create_app(): 
     app = Flask(__name__)
@@ -10,6 +12,11 @@ def create_app():
         title = 'Api Development', 
         description = ' Build and Testing API Development'
     )
+
+    ns = Namespace('hello', description='Hello API Operation')
+    ns = Namespace('items', description='Item Operations')
+    
+    api.add_namespace(ns)
 
     from .controllers.hello_controller import ns as hello_namespace
     api.add_namespace(hello_namespace)
